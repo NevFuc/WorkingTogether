a28db84bb3eb62772a5f12be6c2872a8f9dfd3f0
Elia-code <84073887+Elia-code@users.noreply.github.com>
2025-10-27 13:19:03 +0100

    Quick fix

--- Aquarium/src/ch/coolesPackage/testingPackage/Main.java
+++ Aquarium/src/ch/coolesPackage/testingPackage/Main.java
@@ -5,9 +5,9 @@ package ch.coolesPackage.testingPackage;
 
 public class Main {
     public static void main(String[] args) {
-        Person nevio = new Person("Nevio", 10);
-        Person elia = new Person("Elia", 10);
-        Person oliver = new Person("Oliver", 10);
+        Person nevio = new Person("Nevio", 10000);
+        Person elia = new Person("Elia", 10000);
+        Person oliver = new Person("Oliver", 10000);
 
         System.out.println("Nevios baddies: " + nevio.anzahlBaddies);
         System.out.println("Elias baddies: " + elia.anzahlBaddies);
