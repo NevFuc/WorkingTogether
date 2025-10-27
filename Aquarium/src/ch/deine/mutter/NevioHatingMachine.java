3464ce32e33f9bc579added421094f7aa65e706a
Elia-code <84073887+Elia-code@users.noreply.github.com>
2025-10-27 13:08:50 +0100

    Verbesserung meiner Klasse

--- Aquarium/src/ch/deine/mutter/NevioHatingMachine.java
+++ Aquarium/src/ch/deine/mutter/NevioHatingMachine.java
@@ -5,8 +5,14 @@ public class NevioHatingMachine {
     public int anzahlBaddies;
 
     public NevioHatingMachine(String name,  int anzahlBaddies) {
+        if(name.equals("Nevio")){
+            this.anzahlBaddies = 0;
+        } else if(name.equals("Elia")){
+            this.anzahlBaddies = 1;
+        } else {
+            this.anzahlBaddies = 100;
+        }
         this.name = name;
-        this.anzahlBaddies = anzahlBaddies;
     }
 
     public void insultNevio(){
