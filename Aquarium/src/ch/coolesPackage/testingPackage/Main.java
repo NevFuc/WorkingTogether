1e680b9cee976b2d9e90b80d660da8eb493decff
Elia-code <84073887+Elia-code@users.noreply.github.com>
2025-10-27 13:17:41 +0100

    Verbesserung meiner Klasse

---
+++ Aquarium/src/ch/coolesPackage/testingPackage/Main.java
@@ -0,0 +1,16 @@
+﻿/*
+* Creator: Elia Brunner
+*/
+package ch.coolesPackage.testingPackage;
+
+public class Main {
+    public static void main(String[] args) {
+        Person nevio = new Person("Nevio", 10);
+        Person elia = new Person("Elia", 10);
+        Person oliver = new Person("Oliver", 10);
+
+        System.out.println("Nevios baddies: " + nevio.anzahlBaddies);
+        System.out.println("Elias baddies: " + elia.anzahlBaddies);
+        System.out.println("Olivers baddies: " + oliver.anzahlBaddies);
+    }
+}
--- Aquarium/src/ch/coolesPackage/testingPackage/Person.java
+++ Aquarium/src/ch/coolesPackage/testingPackage/Person.java
@@ -1,16 +1,16 @@
-﻿package ch.deine.mutter;
+﻿package ch.coolesPackage.testingPackage;
 
-public class NevioHatingMachine {
+public class Person {
     public String name;
     public int anzahlBaddies;
 
-    public NevioHatingMachine(String name,  int anzahlBaddies) {
+    public Person(String name, int anzahlBaddies) {
         if(name.equals("Nevio")){
             this.anzahlBaddies = 0;
         } else if(name.equals("Elia")){
             this.anzahlBaddies = 1;
         } else {
-            this.anzahlBaddies = 100;
+            this.anzahlBaddies = anzahlBaddies;
         }
         this.name = name;
     }
