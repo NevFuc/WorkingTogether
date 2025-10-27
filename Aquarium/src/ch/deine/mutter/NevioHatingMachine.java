a41ba70671ddb1e253dbd03438cf808dc5c1c841
Elia-code <84073887+Elia-code@users.noreply.github.com>
2025-10-27 12:43:14 +0100

    New Branch added

---
+++ Aquarium/src/ch/deine/mutter/NevioHatingMachine.java
@@ -0,0 +1,15 @@
+﻿package ch.deine.mutter;
+
+public class NevioHatingMachine {
+    public String name;
+    public int anzahlBaddies;
+
+    public NevioHatingMachine(String name,  int anzahlBaddies) {
+        this.name = name;
+        this.anzahlBaddies = anzahlBaddies;
+    }
+
+    public void insultNevio(){
+        System.out.println("Fick dich Nevio ---|---");
+    }
+}
